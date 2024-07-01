e41f603f031933e54298956ca0d462a0bd80c9db
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-01 16:52:38 +0900

    タブを追加

--- frontend/app/[username]/layout.tsx
+++ frontend/app/[username]/layout.tsx
@@ -1,14 +1,34 @@
 import { Header } from '@/components/header'
+import type { MenuTab } from '@/components/menu-tabs'
+import { MenuTabs } from '@/components/menu-tabs'
+import { Separator } from '@/components/ui/separator'
 
 interface UserLayoutProps {
+  params: { username: string }
   children: React.ReactNode
 }
 
-export default function Layout({ children }: UserLayoutProps) {
+export default function UserLayout({ params, children }: UserLayoutProps) {
+  const username = decodeURIComponent(params.username)
+
+  const tabs: MenuTab[] = [
+    { label: 'Reviews', value: 'reviews', href: `/${username}` },
+    { label: 'Likes', value: 'likes', href: `/${username}/likes` },
+  ]
+
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="container flex-1">
+      <main className="container flex flex-col gap-6">
+        {/* ユーザー情報 */}
+        <div className="mt-6">
+          <p>ユーザー情報</p>
+        </div>
+        {/* タブ */}
+        <div>
+          <MenuTabs tabs={tabs} />
+          <Separator />
+        </div>
         <div className="mx-auto sm:w-4/6">{children}</div>
       </main>
     </div>
