97f55701088f3d9a9659f4224e876e756fec036f
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-02 18:39:27 +0900

    zodを導入

--- frontend/types/api/user.ts
+++ frontend/types/api/user.ts
@@ -1,45 +1,41 @@
-import type { User } from '@/types/user'
+import * as z from 'zod'
 
-export type ApiUser = {
-  username: string
-  immutable_id: string
-  display_name: string
-  avatar_url?: string
-  bio?: string
-  followers_count: number
-  following_count: number
-  created_at: string
-  updated_at: string
-}
+import { isUser, userNameSchema, type User } from '@/types/user'
+
+export const apiUserSchema = z.object({
+  username: userNameSchema,
+  immutable_id: z.string().uuid(),
+  display_name: z.string(),
+  avatar_url: z.string().optional(),
+  bio: z.string().optional(),
+  followers_count: z.number(),
+  following_count: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+})
+
+export type ApiUser = z.infer<typeof apiUserSchema>
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isApiUser(obj: any): obj is ApiUser {
-  return (
-    typeof obj === 'object' &&
-    obj !== null &&
-    typeof obj.username === 'string' &&
-    typeof obj.immutable_id === 'string' &&
-    typeof obj.display_name === 'string' &&
-    (typeof obj.avatar_url === 'undefined' ||
-      typeof obj.avatar_url === 'string') &&
-    (typeof obj.bio === 'undefined' || typeof obj.bio === 'string') &&
-    typeof obj.followers_count === 'number' &&
-    typeof obj.following_count === 'number' &&
-    typeof obj.created_at === 'string' &&
-    typeof obj.updated_at === 'string'
-  )
+  return apiUserSchema.safeParse(obj).success
 }
 
-export function toUser(user: ApiUser): User {
-  return {
-    username: user.username,
-    immutableId: user.immutable_id,
-    displayName: user.display_name,
-    avatarUrl: user.avatar_url,
-    bio: user.bio,
-    followersCount: user.followers_count,
-    followingCount: user.following_count,
-    createdAt: new Date(user.created_at),
-    updatedAt: new Date(user.updated_at),
+export function toUser(apiUser: ApiUser): User {
+  const user = {
+    username: apiUser.username,
+    immutableId: apiUser.immutable_id,
+    displayName: apiUser.display_name,
+    avatarUrl: apiUser.avatar_url,
+    bio: apiUser.bio,
+    followersCount: apiUser.followers_count,
+    followingCount: apiUser.following_count,
+    createdAt: new Date(apiUser.created_at),
+    updatedAt: new Date(apiUser.updated_at),
   }
+
+  if (!isUser(user)) {
+    throw new Error('Invalid user data')
+  }
+  return user
 }
