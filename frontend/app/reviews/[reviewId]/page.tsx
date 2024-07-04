d4bb682cab9b1bda3ddbcd59d2facf52248efee6
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-04 15:07:16 +0900

    レビューページを追加

--- frontend/app/reviews/[reviewId]/page.tsx
+++ frontend/app/reviews/[reviewId]/page.tsx
@@ -1,4 +1,10 @@
+import { FollowButton } from '@/components/follow-button'
+import { LikeButton } from '@/components/reviews/like-button'
+import { TimeStamp } from '@/components/timestamp'
+import { Separator } from '@/components/ui/separator'
+import { UserAvatar } from '@/components/user-avatar'
 import { getReview } from '@/service/reviews/get-review'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 interface ReviewPageProps {
@@ -12,11 +18,35 @@ export default async function ReviewPage({ params }: ReviewPageProps) {
   if (!review) {
     return notFound()
   }
-  console.log(review)
+
+  const pathToUser = `/users/${review.user.username}`
 
   return (
-    <section>
-      <h1>レビュー詳細ページ</h1>
-    </section>
+    <div className="flex justify-center">
+      <div className="mb-16 flex flex-col gap-8 sm:w-7/12">
+        <p>{review.content}</p>
+        <Separator />
+        {/* 投稿ユーザーの情報 */}
+        <section className="sm:text-md flex gap-2 text-sm text-zinc-400 dark:text-zinc-400">
+          <Link href={pathToUser}>
+            <UserAvatar user={review.user} />
+          </Link>
+          <div className="flex flex-col">
+            <Link href={pathToUser}>{review.user.displayName}</Link>
+            <div className="flex items-center gap-2">
+              <TimeStamp date={review.createdAt} />
+            </div>
+          </div>
+          <div className="ml-4">
+            <FollowButton user={review.user} />
+          </div>
+        </section>
+      </div>
+      <div className="fixed bottom-0 flex w-full flex-col pr-0 pt-16 md:sticky md:top-0 md:h-screen md:w-3/12 md:pl-8">
+        <div className="z-10 flex h-16 items-center justify-center border-t border-zinc-700 bg-background dark:border-zinc-700 sm:h-20 sm:rounded-md sm:border">
+          <LikeButton review={review} />
+        </div>
+      </div>
+    </div>
   )
 }
