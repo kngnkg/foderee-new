89d3d3384cc77198e263fd4abb521192e9f0cf3f
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-12 17:02:57 +0900

    レビューコンテンツを表示するコンポーネントを追加

---
+++ frontend/components/reviews/review-content.tsx
@@ -0,0 +1,45 @@
+import { AlbumArt } from '@/components/album-art'
+import { FollowButton } from '@/components/follow-button'
+import { Content } from '@/components/reviews/content'
+import { TimeStamp } from '@/components/timestamp'
+import { Separator } from '@/components/ui/separator'
+import { UserCard } from '@/components/user-card'
+import type { Review } from '@/types/review'
+
+interface ReviewContentProps {
+  review: Review
+}
+export const ReviewContent: React.FC<ReviewContentProps> = ({ review }) => {
+  return (
+    <article className="flex flex-col gap-4">
+      {/* アルバム情報 */}
+      <section className="mt-8">
+        <AlbumArt album={review.album} />
+      </section>
+      {/* タイトル */}
+      <section className="my-2">
+        <h1 className="text-3xl font-extrabold md:text-5xl">{review.title}</h1>
+      </section>
+      {/* 投稿ユーザーの情報 */}
+      <Separator />
+      <section className="flex items-center justify-between gap-2">
+        <div className="flex flex-col">
+          <UserCard user={review.user}>
+            <TimeStamp
+              className="text-zinc-400 dark:text-zinc-400"
+              date={review.createdAt}
+            />
+          </UserCard>
+        </div>
+        <div className="ml-4">
+          <FollowButton user={review.user} />
+        </div>
+      </section>
+      <Separator />
+      {/* 本文 */}
+      <section>
+        <Content data={review.content} />
+      </section>
+    </article>
+  )
+}
