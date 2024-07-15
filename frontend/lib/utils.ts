2e90fb1168043deb0ff78e1d01ab12a6349ef888
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-15 14:19:07 +0900

    addPaginationParamsを移動

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,32 +1,6 @@
-import type { PaginationParams, SearchParams } from '@/types/pagination'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function addPaginationParams(
-  url: string,
-  params: PaginationParams,
-): string {
-  if (params.offset || params.limit) {
-    url += '?'
-    if (params.offset) {
-      url += `offset=${params.offset}&`
-    }
-    if (params.limit) {
-      url += `limit=${params.limit}`
-    }
-    url = url.replace(/&$/, '')
-  }
-  return url
-}
-
-export function addSearchParams(url: string, params: SearchParams): string {
-  const urlWithPagination = addPaginationParams(url, {
-    offset: params.offset,
-    limit: params.limit,
-  })
-  return `${urlWithPagination}&q=${params.q}`
 }
