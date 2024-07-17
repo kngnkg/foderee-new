a39c0abee3732ab8d1db01e64cb7def499d2a395
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-17 14:20:46 +0900

    不要な関数を削除

--- frontend/lib/transform/review.ts
+++ frontend/lib/transform/review.ts
@@ -2,13 +2,12 @@ import { toAlbum } from '@/lib/transform/album'
 import { toUser } from '@/lib/transform/user'
 import type { ApiReview } from '@/types/api/review'
 import type { Review } from '@/types/review'
-import { validateReview } from '@/types/review'
 
 export function toReview(
   apiReview: ApiReview,
   apiAlbum: SpotifyApi.SingleAlbumResponse,
 ): Review {
-  const review = {
+  return {
     reviewId: apiReview.review_id,
     publishedStatus: apiReview.published_status,
     album: toAlbum(apiAlbum),
@@ -19,9 +18,4 @@ export function toReview(
     createdAt: new Date(apiReview.created_at),
     updatedAt: new Date(apiReview.updated_at),
   }
-
-  if (!validateReview(review)) {
-    throw new Error(`Invalid review data: ${JSON.stringify(review)}`)
-  }
-  return review
 }
