26157f57acfd9785e69e791347b5bdafff321c87
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-21 15:21:30 +0900

    handleErrorを追加

--- frontend/__tests__/app/api/response.test.ts
+++ frontend/__tests__/app/api/response.test.ts
@@ -2,8 +2,9 @@
  * @jest-environment node
  */
 
-import { errResponse } from '@/app/api/response'
+import { errResponse, handleError } from '@/app/api/response'
 import { BffErrorType } from '@/types/bff-error'
+import { AppError, AppErrorType } from '@/types/error'
 
 describe('errResponse', () => {
   it('エラータイプが正しくセットされること', async () => {
@@ -98,5 +99,133 @@ describe('errResponse', () => {
         type: BffErrorType.InternalServerError,
       })
     })
+  })
+})
+
+describe('handleError', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('AppErrorの場合', () => {
+    it('リクエストが不正な場合はBadRequestを返す', async () => {
+      const consoleErrorSpy = jest
+        .spyOn(console, 'error')
+        .mockImplementation(() => {})
+      const invalidRequestError = new AppError(
+        'Invalid query parameters',
+        AppErrorType.InvalidRequestError,
+      )
+
+      const response = handleError(invalidRequestError)
+      const body = await response.json()
+
+      expect(consoleErrorSpy).not.toHaveBeenCalled()
+      expect(response.status).toBe(400)
+      expect(body).toEqual({
+        message: 'Invalid query parameters',
+        type: BffErrorType.BadRequest,
+      })
+    })
+
+    it('エンティティが見つからなかった場合はEntityNotFoundを返す', async () => {
+      const consoleErrorSpy = jest
+        .spyOn(console, 'error')
+        .mockImplementation(() => {})
+      const entityNotFoundError = new AppError(
+        'Entity not found',
+        AppErrorType.EntityNotFoundError,
+      )
+
+      const response = handleError(entityNotFoundError)
+      const body = await response.json()
+
+      expect(consoleErrorSpy).not.toHaveBeenCalled()
+      expect(response.status).toBe(404)
+      expect(body).toEqual({
+        message: 'Entity not found',
+        type: BffErrorType.EntityNotFound,
+      })
+    })
+
+    it('UnknownErrorの場合はコンソールに出力してInternalServerErrorを返す', async () => {
+      const consoleErrorSpy = jest
+        .spyOn(console, 'error')
+        .mockImplementation(() => {})
+      const otherAppError = new AppError(
+        'other app error',
+        AppErrorType.UnknownError,
+      )
+
+      const response = handleError(otherAppError)
+      const body = await response.json()
+
+      expect(consoleErrorSpy).toHaveBeenCalledWith(
+        'AppError: other app error',
+        otherAppError.stack, // スタックトレースも出力する
+      )
+      expect(response.status).toBe(500)
+      expect(body).toEqual({
+        message: 'Internal Server Error',
+        type: BffErrorType.InternalServerError,
+      })
+    })
+  })
+
+  it('その他のAppErrorの場合はコンソールに出力してInternalServerErrorを返す', async () => {
+    const consoleErrorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {})
+    const otherAppError = new AppError(
+      'other app error',
+      AppErrorType.SpotifyError,
+    )
+
+    const response = handleError(otherAppError)
+    const body = await response.json()
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith('AppError: other app error')
+    expect(response.status).toBe(500)
+    expect(body).toEqual({
+      message: 'Internal Server Error',
+      type: BffErrorType.InternalServerError,
+    })
+  })
+})
+
+it('AppError以外のエラーの場合はコンソールに出力してInternalServerErrorを返す', async () => {
+  const consoleErrorSpy = jest
+    .spyOn(console, 'error')
+    .mockImplementation(() => {})
+  const otherError = new Error('other error')
+
+  const response = handleError(otherError)
+  const body = await response.json()
+
+  expect(consoleErrorSpy).toHaveBeenCalledWith(
+    'error: other error',
+    otherError.stack, // スタックトレースも出力する
+  )
+  expect(response.status).toBe(500)
+  expect(body).toEqual({
+    message: 'Internal Server Error',
+    type: BffErrorType.InternalServerError,
+  })
+})
+
+it('Error型以外が投げられた場合はコンソールに出力してInternalServerErrorを返す', async () => {
+  const consoleErrorSpy = jest
+    .spyOn(console, 'error')
+    .mockImplementation(() => {})
+  const unknownError = 'unknown error'
+
+  const response = handleError(unknownError)
+  const body = await response.json()
+
+  expect(consoleErrorSpy).toHaveBeenCalledWith('unknown error: ', unknownError)
+  expect(response.status).toBe(500)
+  expect(body).toEqual({
+    message: 'Internal Server Error',
+    type: BffErrorType.InternalServerError,
   })
 })
