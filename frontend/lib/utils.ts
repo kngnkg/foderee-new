dfdeef5c5d11067cc4d942c70356c3a83c2e2aa5
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-23 16:18:29 +0900

    waitを追加

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function wait(second: number) {
+  return new Promise((resolve) => setTimeout(resolve, 1000 * second))
+}
