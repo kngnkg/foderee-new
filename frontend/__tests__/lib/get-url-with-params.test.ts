ab8a9a7c75b570dd83724ba59c14707ed3c3415d
kngnkg <kawata.ryo.0518@gmail.com>
2024-07-24 09:56:35 +0900

    getURLWithSearchParamsを追加

--- frontend/__tests__/lib/get-url-with-params.test.ts
+++ frontend/__tests__/lib/get-url-with-params.test.ts
@@ -1,4 +1,7 @@
-import { getURLWithPaginationParams } from '@/lib/get-url-with-params'
+import {
+  getURLWithPaginationParams,
+  getURLWithSearchParams,
+} from '@/lib/get-url-with-params'
 
 describe('getURLWithPaginationParams', () => {
   it('次のページのoffsetが正しく設定される', async () => {
@@ -18,7 +21,7 @@ describe('getURLWithPaginationParams', () => {
     ).toBe(expected)
   })
 
-  it('以前のページが既に最後のページに到達していた場合はnullを返す', async () => {
+  it('直前のページが既に最後のページに到達していた場合はnullを返す', async () => {
     const endpoint = 'http://example.com/api/users/testuser/followees'
     const limit = 20
     const pageIndex = 3
@@ -47,6 +50,76 @@ describe('getURLWithPaginationParams', () => {
 
     expect(
       getURLWithPaginationParams(endpoint, limit, pageIndex, previousPage),
+    ).toBe(expected)
+  })
+})
+
+describe('getURLWithSearchParams', () => {
+  it('検索クエリと次のページのoffsetが正しく設定される', async () => {
+    const endpoint = 'http://example.com/api/albums'
+    const query = 'test'
+    const limit = 20
+    const pageIndex = 2
+    const previousPage = {
+      offset: 20,
+      limit: 20,
+      total: 60,
+    }
+
+    const expected = `${endpoint}?q=test&offset=40&limit=20`
+
+    expect(
+      getURLWithSearchParams(endpoint, query, limit, pageIndex, previousPage),
+    ).toBe(expected)
+  })
+
+  it('検索クエリが空文字の場合はnullを返す', async () => {
+    const endpoint = 'http://example.com/api/albums'
+    const query = ''
+    const limit = 20
+    const pageIndex = 2
+    const previousPage = {
+      offset: 20,
+      limit: 20,
+      total: 60,
+    }
+
+    expect(
+      getURLWithSearchParams(endpoint, query, limit, pageIndex, previousPage),
+    ).toBeNull()
+  })
+
+  it('直前のページが既に最後のページに到達していた場合はnullを返す', async () => {
+    const endpoint = 'http://example.com/api/albums'
+    const query = 'test'
+    const limit = 20
+    const pageIndex = 3
+    const previousPage = {
+      offset: 60,
+      limit: 20,
+      total: 60,
+    }
+
+    expect(
+      getURLWithSearchParams(endpoint, query, limit, pageIndex, previousPage),
+    ).toBeNull()
+  })
+
+  it('最初のページの場合はoffsetが0に設定される', async () => {
+    const endpoint = 'http://example.com/api/albums'
+    const query = 'test'
+    const limit = 20
+    const pageIndex = 0
+    const previousPage = {
+      offset: 0,
+      limit: 20,
+      total: 60,
+    }
+
+    const expected = `${endpoint}?q=test&offset=0&limit=20`
+
+    expect(
+      getURLWithSearchParams(endpoint, query, limit, pageIndex, previousPage),
     ).toBe(expected)
   })
 })
--- frontend/lib/get-url-with-params.ts
+++ frontend/lib/get-url-with-params.ts
@@ -1,4 +1,7 @@
-import { addPaginationParams } from '@/lib/add-pagination-params'
+import {
+  addPaginationParams,
+  addSearchParams,
+} from '@/lib/add-pagination-params'
 import type { Pagination } from '@/types/pagination'
 
 export const getURLWithPaginationParams = (
@@ -19,4 +22,30 @@ export const getURLWithPaginationParams = (
 
   const nextOffset = previousPage.offset + previousPage.limit
   return addPaginationParams(endpoint, { offset: nextOffset, limit })
+}
+
+export const getURLWithSearchParams = (
+  endpoint: string,
+  query: string,
+  limit: number,
+  pageIndex: number,
+  previousPage: Pagination,
+): string | null => {
+  // 検索クエリがない場合
+  if (query === '') {
+    return null
+  }
+
+  // 最後に到達した場合
+  if (previousPage && previousPage.offset >= previousPage.total) {
+    return null
+  }
+
+  // 最初のページの場合
+  if (pageIndex === 0) {
+    return addSearchParams(endpoint, { q: query, offset: 0, limit })
+  }
+
+  const nextOffset = previousPage.offset + previousPage.limit
+  return addSearchParams(endpoint, { q: query, offset: nextOffset, limit })
 }
